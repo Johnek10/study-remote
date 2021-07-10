2d8d1ef298cb46c0b9bb196efe4baecba050094a
Jan Sygnowski <jansyg10@gmail.com>
2021-07-10 14:59:24 +0200

    class component with state finished

--- src/components/organims/UsersList/UsersList.js
+++ src/components/organims/UsersList/UsersList.js
@@ -7,11 +7,47 @@ import { Wrapper } from './UserList.styles';
   return <div>JanM</div>;
 }; */
 
+
+const mockApi=(success)=>{
+  return new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+      if(users){
+        resolve([...users]);
+      }
+      else{
+        reject({message: 'Error'});
+      }
+    },2000);
+  });
+}
+
+
+
+
+
+
 class UsersList extends React.Component {
   state = {
     title: true,
-    users,
+    users: [],
+    isLoading: false,
   };
+
+  componentDidMount(){
+    this.setState({isLoading: true});
+    mockApi()
+    .then(data=>{
+      this.setState({isLoading: false});
+      this.setState( {users: data});
+    })
+    .catch(error=>console.log(error))
+  }
+  componentDidUpdate(prevstate){
+    if(prevstate.isLoading !=this.state.isLoading){
+      console.log('dupa changed');
+    }
+  }
+
   deleteUser = (name) => {
     const filterusers = this.state.users.filter(user => user.name !== name);
     console.log(filterusers);
@@ -21,7 +57,7 @@ class UsersList extends React.Component {
     console.log(this);
     return (
       <Wrapper>
-        <h3>{this.state.title ? `User's list` : 'Students list'}</h3>
+        <h3>{this.state.isLoading ? 'Loading...' : 'Students list'}</h3>
         <ul>
           {this.state.users.map((userData, index) => (
             <UsersListItem index={index} userData={userData} deleteUser={this.deleteUser} />
