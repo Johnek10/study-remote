8f6cf3e8eba98494ea02f1f3207341efc41ef7bf
Jan Sygnowski <jansyg10@gmail.com>
2021-10-31 22:06:42 +0100

    changed to class component //bug

--- src/components/atoms/Button/Button.js
+++ src/components/atoms/Button/Button.js
@@ -2,11 +2,7 @@ import React from 'react';
 import { ReactComponent as DeleteIcon } from 'assets/icons/deleteIcon.svg';
 import { StyledButton } from './Button.styles.js';
 
-/*
-const hello = () => {
-  alert('Dupa po prostu');
-};
-*/
+
 const Button = (props) => (
   <StyledButton {...props}>
     <DeleteIcon />
--- src/components/organims/UsersList/UsersList.js
+++ src/components/organims/UsersList/UsersList.js
@@ -2,21 +2,28 @@ import React from 'react';
 import users from 'data/users.js';
 import UsersListItem from 'components/molecules/UsersListItem/UsersListItem';
 import { Wrapper } from './UserList.styles';
-
-/* const fun = () => {
-  return <div>JanM</div>;
-}; */
-
-const UsersList = () => (
+class UsersList extends React.Component{
+  render(){
+    return(
+      <Wrapper>
+    <ul>
+      {users.map((userData, index) => (
+        <UsersListItem index={index} userData={userData} />
+      ))}
+    </ul>
+  </Wrapper>
+    )
+  }
+}
+/* const UsersList = () => (
   <Wrapper>
     <ul>
-   
       {users.map((userData, index) => (
         <UsersListItem index={index} userData={userData} />
       ))}
       
     </ul>
   </Wrapper>
-);
+); */
 
 export default UsersList;
