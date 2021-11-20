2e50df9fc980ed7613250050ca1251eddefc94f3
Jan Sygnowski <jansyg10@gmail.com>
2021-11-20 18:26:04 +0100

    added validate checkbox

--- src/views/AddUser.js
+++ src/views/AddUser.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useReducer } from 'react';
+import React, {useContext, useReducer } from 'react';
 import FormField from 'components/molecules/FormField/FormField';
 import { Button } from 'components/atoms/Button/Button';
 import { ViewWrapper } from 'components/molecules/ViewWrapper/ViewWrapper';
@@ -6,20 +6,38 @@ import { Title } from 'components/atoms/Title/Title';
 import { UsersContext } from 'providers/UsersProviders';
 
 const AddUser = () => {
-  const startFormState={
+  const actionTypes = {
+    inputChange: 'INPUT CHANGE',
+    clearValues: 'CLEAR FORMVALUES',
+    consentToggle: 'CONSENT TOGGLE',
+    error: 'ERROR',
+  };
+  const startFormState = {
     name: '',
     attendance: '',
     average: '',
-  }
+    consent: false,
+    error: '',
+  };
   const reducer = (state, action) => {
-    switch(action.type){
-      case 'INPUT CHANGE':
+    switch (action.type) {
+      case actionTypes.inputChange:
         return {
           ...state,
-          [action.field]: action.value
-        }
-      case 'CLEAR FORMVALUES':
+          [action.field]: action.value,
+        };
+      case actionTypes.clearValues:
         return startFormState;
+      case actionTypes.consentToggle:
+        return {
+          ...state,
+          consent: !state.consent,
+        };
+      case actionTypes.error:
+        return {
+          ...state,
+          error: action.errorValue,
+        };
       default:
         return state;
     }
@@ -29,22 +47,26 @@ const AddUser = () => {
   const [formsValue, dispatch] = useReducer(reducer, startFormState);
   const handleName = (e) => {
     dispatch({
-      type: 'INPUT CHANGE',
+      type: actionTypes.inputChange,
       field: e.target.name,
-      value: e.target.value
-    })
-    };
+      value: e.target.value,
+    });
+  };
 
   const handleSubmitUser = (e) => {
     e.preventDefault();
-    context.handleAddUser(formsValue);
-    dispatch({type : 'CLEAR FORMVALUES'});
+    if (formsValue.consent) {
+      context.handleAddUser(formsValue);
+      dispatch({ type: actionTypes.clearValues });
+    } else {
+      dispatch({ type: actionTypes.error, errorValue: 'Nie wyraziles zgody' });
+    }
   };
 
   return (
     <ViewWrapper as="form" onSubmit={handleSubmitUser}>
       <Title>Add User</Title>
-<FormField
+      <FormField
         label="Name"
         id="name"
         name="name"
@@ -65,6 +87,16 @@ const AddUser = () => {
         value={formsValue.average}
         onChange={handleName}
       />
+      <FormField
+        label="Consent"
+        id="consent"
+        name="constent"
+        type="checkbox"
+        onChange={() => {
+          dispatch({ type: 'CONSENT TOGGLE' });
+        }}
+      ></FormField>
+      <p>{formsValue.consent ? 'OK' : formsValue.error}</p>
       <Button type="submit">Add</Button>
     </ViewWrapper>
   );
